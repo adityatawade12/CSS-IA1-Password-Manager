ce668f4b948ac390afd66516e15580f44f17ca00
Omkar Sawant <omkar18@somaiya.edu>
2022-02-16 23:40:33 +0530

    jquery added to add.js

--- app/js/add.js
+++ app/js/add.js
@@ -1,3 +1,66 @@
+$("#back").on("click",function(){
+    ipcRenderer.send('back')
+})
+
+$("#catType").on('change',function(){
+    
+    if($("#catType").val()=="Existing"){
+        $("#Existing").css({"display":"initial"})
+        $("#New").css({"display":"none"})
+        $("#Existing").val("0");
+    }else{
+        $("#New").css({"display":"initial"})
+        $("#Existing").css({"display":"none"})
+        $("#New").val("");
+    }
+    enableSave()
+})
+
+$("#Existing").on('change',function(){
+    enableSave()
+})
+
+
+$("#email").on("input",function(){
+    enableSave()
+})
+
+$("#password2").on("input",function(){
+    enableSave()
+})
+
+$("#show1").on("click",function(){
+    if($("#password2").attr("type")=="text"){
+        $("#password2").attr("type","password")
+    }else{
+        $("#password2").attr("type","text")
+    }
+    
+
+})
+
+$("#New").on("input", function(){
+    
+    enableSave()
+    
+});
+
+ipcRenderer.on("update",(e,object)=>{
+    ipcRenderer.send('getData')
+    clearAll()
+})
+
+$("#save").on('click',function(){
+   
+    ipcRenderer.send("createNew",{
+        category:$("#"+$("#catType").val()).val(),
+        username:$("#email").val(),
+        password:$("#password2").val()
+    })
+    clearAll()
+
+})
+
 function enableSave() {
     console.log($("#Existing").val())
     if($("#catType").val()=="Existing" && $("#Existing").val()!="0" && $("#Existing").val()!=null ){
